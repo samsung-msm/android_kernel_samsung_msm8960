4db236d1d377cb7fa8e85180c0ae66051c450715
Jack Pham <jackp@codeaurora.org>
2012-09-26 10:13:48 -0700

    usb: gadget: Fix android compilation error
    Recent audio composition support in Android gadget driver depends
    on CONFIG_SND_PCM. Conditionally include the audio function only
    if this is defined.
    
    Change-Id: I0e61687e93e2c4002a3df8618147278f7df6dc7d
    Signed-off-by: Jack Pham <jackp@codeaurora.org>
    
    Conflicts:
    	drivers/usb/gadget/android.c

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -87,8 +87,10 @@
 #ifdef CONFIG_TARGET_CORE
 #include "f_tcm.c"
 #endif
+#ifdef CONFIG_SND_PCM
 #include "u_uac1.c"
 #include "f_uac1.c"
+#endif
 
 MODULE_AUTHOR("Mike Lockwood");
 MODULE_DESCRIPTION("Android Composite USB Driver");
@@ -873,6 +875,7 @@ static struct android_usb_function mbim_function = {
 };
 #endif
 
+#ifdef CONFIG_SND_PCM
 /* PERIPHERAL AUDIO */
 static int audio_function_bind_config(struct android_usb_function *f,
 					  struct usb_configuration *c)
@@ -884,6 +887,7 @@ static struct android_usb_function audio_function = {
 	.name		= "audio",
 	.bind_config	= audio_function_bind_config,
 };
+#endif
 
 /* DIAG */
 static char diag_clients[32];	    /*enabled DIAG clients- "diag[,diag_mdm]" */
@@ -2055,7 +2059,9 @@ static struct android_usb_function *supported_functions[] = {
 	&ecm_qc_function,
 	&rndis_qc_function,
 #endif
+#ifdef CONFIG_SND_PCM
 	&audio_function,
+#endif
 	&rmnet_smd_function,
 	&rmnet_sdio_function,
 	&rmnet_smd_sdio_function,
