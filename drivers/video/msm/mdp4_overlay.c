c7fc3890dedbb6fec08e4e5252ac528572c7449d
Kuogee Hsieh <khsieh@codeaurora.org>
2013-02-22 15:46:31 -0800

    msm_fb: display: calculate mdp clock including borderfill pipe
    For hdmi case, mdp clock calculation needs including borderfill
    pipe to prevent dtv underrun while system is in suspend.
    
    Crs-fixed: 449027
    Change-Id: I98789343317d5cfdf1f56e496cecb4dd663c907f
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/mdp4_overlay.c
    	drivers/video/msm/mdp4_overlay_dtv.c

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -3286,13 +3286,12 @@ static int mdp4_axi_port_write_client_mixer(int mixer)
 	return port;
 }
 
-int mdp4_overlay_mdp_perf_req(struct msm_fb_data_type *mfd,
-			      struct mdp4_overlay_pipe *plist)
+int mdp4_overlay_mdp_perf_req(struct msm_fb_data_type *mfd)
 {
 	u32 worst_mdp_clk = 0;
 	int i;
 	struct mdp4_overlay_perf *perf_req = &perf_request;
-	struct mdp4_overlay_pipe *pipe = plist;
+	struct mdp4_overlay_pipe *pipe;
 	int verysmallarea =0;
 	int yuvcount =0;
 	int src_h_total = 0;
@@ -3312,10 +3311,7 @@ int mdp4_overlay_mdp_perf_req(struct msm_fb_data_type *mfd,
 		return ret;
 	}
 
-	if (!plist) {
-		pr_err("%s: plist is null!\n", __func__);
-		return ret;
-	}
+	pipe = ctrl->plist;
 
 	for (i = 0; i < MDP4_MIXER_MAX; i++)
 		perf_req->use_ov_blt[i] = 0;
@@ -4220,7 +4216,7 @@ int mdp4_overlay_play(struct fb_info *info, struct msmfb_overlay_data *req)
 		}
 	}
 
-	mdp4_overlay_mdp_perf_req(mfd, ctrl->plist);
+	mdp4_overlay_mdp_perf_req(mfd);
 #if defined(CONFIG_FEATURE_FLIPLR)
 	pipe->mfd = mfd;
 #endif
