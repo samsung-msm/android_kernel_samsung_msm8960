8038469f365765c53110e738a02da1b0eb1c434f
Dave Daynard <nardholio@gmail.com>
2014-05-04 05:23:02 -0400

    The Samsung is strong with this one

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -356,8 +356,6 @@ static void msm_fb_remove_sysfs(struct platform_device *pdev)
 	sysfs_remove_group(&mfd->fbi->dev->kobj, &msm_fb_attr_group);
 }
 
-static void bl_workqueue_handler(struct work_struct *work);
-
 static void msm_fb_shutdown(struct platform_device *pdev)
 {
        struct msm_fb_data_type *mfd = platform_get_drvdata(pdev);
@@ -1879,7 +1877,6 @@ static int msm_fb_release_all(struct fb_info *info, boolean is_all)
 	}
 
 	pm_runtime_put(info->dev);
-	pr_debug("Node: %d user: %d op_mode %d ref_count: %d", info->node, user, mfd->op_enable,mfd->ref_cnt);
 	return ret;
 }
 
